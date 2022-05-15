a3f00fcfe3acd0f08f6eb3bdb5482f56be9784e8
Lseonghak <dltjdgkr6220@gmail.com>
2022-05-15 16:07:05 +0900

    게임 준비화면 구현

--- app/src/main/java/com/example/good_bad_game/Home.java
+++ app/src/main/java/com/example/good_bad_game/Home.java
@@ -2,7 +2,10 @@ package com.example.good_bad_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button start = findViewById(R.id.btn_start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ReadyGame.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
