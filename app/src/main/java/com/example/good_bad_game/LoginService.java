90891fec34124c08f78c69cd6c98a76d7a2bf332
Lseonghak <dltjdgkr6220@gmail.com>
2022-05-18 17:55:54 +0900

    로그인 call error 수정

--- app/src/main/java/com/example/good_bad_game/LoginService.java
+++ app/src/main/java/com/example/good_bad_game/LoginService.java
@@ -1,9 +1,8 @@
 package com.example.good_bad_game;
 
-import android.telecom.Call;
-
 import java.util.List;
 
+import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -11,7 +10,7 @@ import retrofit2.http.POST;
 public interface LoginService {
 
     @GET("users/")
-    Call<List<Login>> getPosts();
+    retrofit2.Call<List<Login>> getPosts();
 
     @POST("users/")
     Call<Post> createPost(@Body Post post);
