5b99f90b8c4c3f24eb48a6fbd0ad58caca066a84
Lseonghak <dltjdgkr6220@gmail.com>
2022-05-19 19:02:21 +0900

    화면 전환 부드럽게 하기

--- app/src/main/java/com/example/good_bad_game/MyItem.java
+++ app/src/main/java/com/example/good_bad_game/MyItem.java
@@ -1,14 +1,83 @@
 package com.example.good_bad_game;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
 
 public class MyItem extends AppCompatActivity {
+
+    private final static String TAG = "MyItem";
+    public TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_item);
+
+        tts = new TextToSpeech(MyItem.this, new TextToSpeech.OnInitListener(){
+
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS){
+                    int result = tts.setLanguage(Locale.KOREA);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+
+                        Toast.makeText(MyItem.this, "지원하지 않는 언어입니다.",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        Button store = findViewById(R.id.btn_store);
+        store.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts.speak("상점", TextToSpeech.QUEUE_FLUSH, null);
+                Intent intent = new Intent(getApplicationContext(), Store.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_left_enter, R.anim.slide_left_exit);
+            }
+        });
+
+        Button home = findViewById(R.id.btn_main);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts.speak("메인화면", TextToSpeech.QUEUE_FLUSH, null);
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit);
+            }
+        });
+
+        Button ranking = findViewById(R.id.btn_ranking);
+        ranking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts.speak("랭킹", TextToSpeech.QUEUE_FLUSH, null);
+                Intent intent = new Intent(getApplicationContext(), Ranking.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit);
+            }
+        });
+
+        Button friend = findViewById(R.id.btn_friend);
+        friend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts.speak("친구", TextToSpeech.QUEUE_FLUSH, null);
+                Intent intent = new Intent(getApplicationContext(), MenuFriend.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit);
+            }
+        });
+
     }
 }
