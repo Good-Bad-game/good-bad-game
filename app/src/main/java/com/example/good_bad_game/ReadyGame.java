127446beb71544eb78e51ed550674e870baaf184
Lseonghak <dltjdgkr6220@gmail.com>
2022-06-09 05:05:26 +0900

    데이터 통신 구현중

--- app/src/main/java/com/example/good_bad_game/ReadyGame.java
+++ app/src/main/java/com/example/good_bad_game/ReadyGame.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.good_bad_game.databinding.ActivityReadyGameBinding;
 import com.example.good_bad_game.loginout.LoginService;
 
 import java.util.Collections;
@@ -29,8 +30,14 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import org.webrtc.SurfaceViewRenderer;
 
 public class ReadyGame extends AppCompatActivity {
+
+    ActivityReadyGameBinding binding;
 
     private String id;
     private String v_type;
--- app/src/main/java/com/example/good_bad_game/home/Home.java
+++ app/src/main/java/com/example/good_bad_game/home/Home.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.good_bad_game.R;
+import com.example.good_bad_game.databinding.ActivityHomeBinding;
 import com.example.good_bad_game.friend.FriendFragment;
 import com.example.good_bad_game.myitem.ItemFragment;
 import com.example.good_bad_game.ranking.RankingFragment;
